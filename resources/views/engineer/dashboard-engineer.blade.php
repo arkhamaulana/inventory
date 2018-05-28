90b7dfcb5ea96f96e631cbdc332f4e759d2e4b42
ladinar <ladinarnanda@gmail.com>
2018-05-28 12:52:57 -0700

    terus update

--- resources/views/engineer/dashboard-engineer.blade.php
+++ resources/views/engineer/dashboard-engineer.blade.php
@@ -4,7 +4,7 @@
 
     <div class="quick-actions_homepage">
       <ul class="quick-actions">
-      	<h1>Dashboard Engineer Update</h1>
+      	<h1>Dashboard Engineer Baru</h1>
 
       </ul>
     </div>
